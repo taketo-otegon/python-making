23f9ea1c63aa450964e19bbbb8a33b784ad4cfa1
Taketo Saeki <taketowinter@gmail.com>
2023-05-02 17:57:17 +0900

    fourier級数展開を求める

--- 2023/5/fourier.py
+++ 2023/5/fourier.py
@@ -2,51 +2,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# 定義する関数
+# 関数 f(x) の定義
 def f(x):
-    return x
+    return np.sin(x) + np.cos(3*x) + np.sin(5*x)
 
 # フーリエ係数の計算
-def fourier_coefficients(f, n_max, L):
-    a0 = (1/L) * np.trapz(f(x), x)
-    an = []
-    bn = []
-    for n in range(1, n_max+1):
-        integrand_cos = lambda x: f(x) * np.cos(2 * np.pi * n * x / L)
-        integrand_sin = lambda x: f(x) * np.sin(2 * np.pi * n * x / L)
-        an.append((1/L) * np.trapz(integrand_cos(x), x))
-        bn.append((1/L) * np.trapz(integrand_sin(x), x))
+def fourier_coefficients(n, T):
+    t = np.linspace(-T/2, T/2, 1000)
+    y = f(t)
+    dt = T/len(t)
+
+    a0 = (2/T) * np.sum(y) * dt
+    an = lambda n: (2/T) * np.sum(y * np.cos(2 * np.pi * n * t / T)) * dt
+    bn = lambda n: (2/T) * np.sum(y * np.sin(2 * np.pi * n * t / T)) * dt
+
     return a0, an, bn
 
-# フーリエ級数の計算
-def fourier_series(x, a0, an, bn, L):
-    series = np.zeros_like(x)
-    series += a0/2
-    for n in range(1, len(an)+1):
-        series += an[n-1] * np.cos(2 * np.pi * n * x / L)
-        series += bn[n-1] * np.sin(2 * np.pi * n * x / L)
-    return series
+# フーリエ級数展開
+def fourier_series(x, n_max, T, a0, an, bn):
+    result = a0/2
+    for n in range(1, n_max + 1):
+        result += an(n) * np.cos(2 * np.pi * n * x / T) + bn(n) * np.sin(2 * np.pi * n * x / T)
+    return result
 
-# 計算パラメータの設定
-L = 2 * np.pi # 周期
-x = np.linspace(-L, L, 1000)
+# パラメータ設定
+n_max = 10
+T = 2 * np.pi
+x = np.linspace(-T/2, T/2, 1000)
 
-# グラフの設定
+# プロット設定
 fig, ax = plt.subplots()
+ax.set_xlim(-T/2, T/2)
+ax.set_ylim(-2, 2)
 line1, = ax.plot(x, f(x), label='f(x)')
-line2, = ax.plot([], [], label='Fourier series')
-
+line, = ax.plot(x, np.zeros_like(x),label='Fourier series')
 ax.legend()
 
-# アニメーションの更新関数
-def update(n_max):
-    a0, an, bn = fourier_coefficients(f, n_max, L)
-    series = fourier_series(x, a0, an, bn, L)
-    line2.set_data(x, series)
-    ax.set_title('n_max = {}'.format(n_max))
 
-# アニメーションの作成
-ani = FuncAnimation(fig, update, frames=range(1,101), interval=100)
+a0, an, bn = fourier_coefficients(n_max, T)
 
-# アニメーションの表示
+def update(num):
+    line.set_ydata(fourier_series(x, num, T, a0, an, bn))
+    ax.set_title('n_max = {}'.format(num))
+    return line,ax
+
+# アニメーション
+ani = FuncAnimation(fig, update, frames=range(n_max+1), interval=500)
+
 plt.show()
